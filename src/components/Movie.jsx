5168605dd46984597055592cc21a5e625d95ce6e
saeree <saeree_club@hotmail.co.th>
2020-04-26 17:39:42 +0700

    อธิบายการใช้งาน use effect ว่าแต่ละอันแตกต่างกันยังไง

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,30 +3,24 @@ import { data as movies } from "../movie.json";
 
 const baseStyle = {
   cursor: "pointer",
-  // border: 'solid 2px black'
+  //border: "solid 2px red",
 };
-
-// cursor:pointer;
 
 export default function Movie() {
   const [select, setSelect] = useState(false);
-  const [style, setStyle] = useState(baseStyle);
+  // const [style, setStyle] = useState[baseStyle];
 
   useEffect(() => {
+    // เอาไว้ทำการเดียวหลัง reload
     console.log("mounted"); // mounted
   }, []);
 
   useEffect(() => {
     console.log("updated", select);
-
-    setStyle({
-      ...baseStyle,
-      border: select ? "solid 2px black" : "",
-    });
-  }, [select]);
+  }, [select]); // state ที่เป็น select ทำการ update
 
   useEffect(() => {
-    console.log("anything updated");
+    console.log("anything update"); // state ทุกตัวทำการ update หมด
   });
 
   function handleClick() {
@@ -40,58 +34,9 @@ export default function Movie() {
         <img
           key={each.id}
           onClick={handleClick}
-          style={style}
           src={"http://localhost:3000/images/" + each.id + ".jpg"}
         />
       ))}
     </div>
   );
 }
-// import React, { useState, useEffect } from "react";
-// import { data as movies } from "../movie.json";
-
-// const baseStyle = {
-//   cursor: "pointer",
-//   //border: "solid 2px red",
-// };
-
-// export default function Movie() {
-//   const [select, setSelect] = useState(false);
-//   const [style, setStyle] = useState[baseStyle];
-
-//   useEffect(() => {
-//     // เอาไว้ทำการเดียวหลัง reload
-//     console.log("mounted"); // mounted
-//   }, []);
-
-//   useEffect(() => {
-//     console.log("updated", select);
-
-//     setStyle({
-//       ...baseStyle,
-//       border: select ? "solid 2px black" : "",
-//     });
-//   }, [select]);
-
-//   useEffect(() => {
-//     console.log("anything update"); // state ทุกตัวทำการ update หมด
-//   });
-
-//   function handleClick() {
-//     setSelect(!select);
-//   }
-
-//   return (
-//     <div>
-//       <h1>Movie</h1>
-//       {movies.map((each) => (
-//         <img
-//           key={each.id}
-//           onClick={handleClick}
-//           style={style}
-//           src={"http://localhost:3000/images/" + each.id + ".jpg"}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
