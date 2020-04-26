aa0c343d78814e1d2db964e0b1fc5ce2a91f7212
saeree <saeree_club@hotmail.co.th>
2020-04-26 16:25:18 +0700

    clear comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Form from "./components/Input";
 import useFormInput from "./use-form-input";
 
 export default function App() {
@@ -16,31 +15,3 @@ export default function App() {
     </div>
   );
 }
-
-// export default function App() {
-//   return (
-//     <div>
-//       <Form />
-//     </div>
-//   );
-// }
-
-// export default function App() {
-//   const [counter, setCounter] = useState(1);
-//   const [number, setNumber] = useState(100); // setNumber เอาไว้ update ค่าของ Number
-
-//   function handleClick() {
-//     setCounter(counter + 1);
-//   }
-
-//   return (
-//     <div>
-//       <h1>App {counter}</h1>
-//       <button onClick={handleClick}>Update</button>
-//       <button onClick={() => setCounter(counter + 1)}>Update</button>
-//       {/* annonymus function การประกาศ function ลอยๆขึ้นมา */}
-//       <hr />
-//     </div>
-//   );
-
-// }
