da1b3fc3ec2ced86cd2efd20a5c55bd4e3760b24
saeree <saeree_club@hotmail.co.th>
2020-04-26 15:54:05 +0700

    use multiple use state

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 export default function App() {
   const [counter, setCounter] = useState(1);
+  const [number, setNumber] = useState(100); // setNumber เอาไว้ update ค่าของ Number
 
   function handleClick() {
     setCounter(counter + 1);
@@ -11,7 +12,8 @@ export default function App() {
     <div>
       <h1>App {counter}</h1>
       <button onClick={handleClick}>Update</button>
-      <button onClick={()=>setCounter(counter + 1);}>Update</button> {/* annonymus function การประกาศ function ลอยๆขึ้นมา */}
+      <button onClick={() => setCounter(counter + 1)}>Update</button>
+      {/* annonymus function การประกาศ function ลอยๆขึ้นมา */}
       <hr />
     </div>
   );
