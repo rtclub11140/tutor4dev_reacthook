1494cfa514d1513372c6d8a1187f9b4a434390a0
saeree <saeree_club@hotmail.co.th>
2020-04-26 16:59:55 +0700

    การใช้ use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-import useFormInput from "./use-form-input";
+// import useFormInput from "./use-form-input";
+import Movie from "./components/Movie.jsx";
 
 export default function App() {
-  const [name, handleNameChange] = useFormInput("John");
-  const [surname, handleSurnameChange] = useFormInput("Doe");
-
   return (
     <div>
-      <input onChange={handleNameChange} value={name} type="text" />
-      <p>{name}</p>
-      <hr />
-      <input onChange={handleSurnameChange} value={surname} type="text" />
-      <p>{surname}</p>
+      <Movie />
     </div>
   );
 }
+
+//  export default function App() {
+//   const [name, handleNameChange] = useFormInput("John");
+//   const [surname, handleSurnameChange] = useFormInput("Doe");
+
+//   return (
+//     <div>
+//       <input onChange={handleNameChange} value={name} type="text" />
+//       <p>{name}</p>
+//       <hr />
+//       <input onChange={handleSurnameChange} value={surname} type="text" />
+//       <p>{surname}</p>
+//     </div>
+//   );
+// }
---
+++ src/components/Movie.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import { data as movies } from "../movie.json";
+
+const baseStyle = {
+  cursor: "pointer",
+  // border: 'solid 2px black'
+};
+
+// cursor:pointer;
+
+export default function Movie() {
+  const [select, setSelect] = useState(false);
+  const [style, setStyle] = useState(baseStyle);
+
+  useEffect(() => {
+    console.log("mounted"); // mounted
+  }, []);
+
+  useEffect(() => {
+    console.log("updated", select);
+
+    setStyle({
+      ...baseStyle,
+      border: select ? "solid 2px black" : "",
+    });
+  }, [select]);
+
+  useEffect(() => {
+    console.log("anything updated");
+  });
+
+  function handleClick() {
+    setSelect(!select);
+  }
+
+  return (
+    <div>
+      <h1>Movie</h1>
+      {movies.map((each) => (
+        <img
+          key={each.id}
+          onClick={handleClick}
+          style={style}
+          src={"http://localhost:3000/images/" + each.id + ".jpg"}
+        />
+      ))}
+    </div>
+  );
+}
+// import React, { useState, useEffect } from "react";
+// import { data as movies } from "../movie.json";
+
+// const baseStyle = {
+//   cursor: "pointer",
+//   //border: "solid 2px red",
+// };
+
+// export default function Movie() {
+//   const [select, setSelect] = useState(false);
+//   const [style, setStyle] = useState[baseStyle];
+
+//   useEffect(() => {
+//     // เอาไว้ทำการเดียวหลัง reload
+//     console.log("mounted"); // mounted
+//   }, []);
+
+//   useEffect(() => {
+//     console.log("updated", select);
+
+//     setStyle({
+//       ...baseStyle,
+//       border: select ? "solid 2px black" : "",
+//     });
+//   }, [select]);
+
+//   useEffect(() => {
+//     console.log("anything update"); // state ทุกตัวทำการ update หมด
+//   });
+
+//   function handleClick() {
+//     setSelect(!select);
+//   }
+
+//   return (
+//     <div>
+//       <h1>Movie</h1>
+//       {movies.map((each) => (
+//         <img
+//           key={each.id}
+//           onClick={handleClick}
+//           style={style}
+//           src={"http://localhost:3000/images/" + each.id + ".jpg"}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
